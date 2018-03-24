7203367cbbb5b3273d3e006c3cde526d461643f2
Berkay <32465888+DevHunterYZ@users.noreply.github.com>
2018-03-24 23:09:10 +0300

    Update RandomForest.py

--- RandomForest.py
+++ RandomForest.py
@@ -25,3 +25,17 @@ df['species'] = pd.Categorical.from_codes(iris.target, iris.target_names)
 
 # İlk 5 satırı görüntüle.
 df.head()
+# Her satır için 0 ile 1 arasında rasgele bir sayı oluşturan yeni bir sütun oluşturun ve
+# bu değer 0,75'ten küçük veya ona eşitse, o hücrenin değerini True olarak ayarlar.
+# aksi takdirde False döndürür. Bu, bazı satırları rastgele bir şekilde atamanın hızlı ve kirli bir yoludur.
+# Eğitim verileri ve bazıları test verileri olarak kullanılabilir.
+df ['is_train'] = np.random.uniform (0, 1, len (df)) <= .75
+
+# En iyi 5 satırı görüntüle
+df.head ()
+# Biri test satırı olan diğeride bir eğitim(train) satırı olan iki yeni veri tablosu oluşturun.
+train, test = df[df['is_train']==True], df[df['is_train']==False]
+# Test ve eğitim veri çerçeveleri için gözlem sayısını göster.
+print('Eğitim verilerindeki gözlem sayısı:', len(train))
+print('Test verilerindeki gözlem sayısı:',len(test))
+
